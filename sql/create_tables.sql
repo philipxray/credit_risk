df1dbe130aba7e2a8be739956791757b06ed1355
Philip Ray <philipxray@gmail.com>
2026-01-29 09:54:11 -0800

    Create credit_features table from credit_usd
    Create credit_features table from credit_usd with selected fields and computed columns.

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,49 @@
 
+DROP TABLE IF EXISTS credit_features;
+
+CREATE TABLE credit_features AS
+SELECT
+    id,
+    limit_bal_usd,
+    sex,
+    education,
+    marriage,
+    age,
+
+    pay_0,
+    pay_2,
+    pay_3,
+    pay_4,
+    pay_5,
+    pay_6,
+
+    (
+        bill_amt1_usd +
+        bill_amt2_usd +
+        bill_amt3_usd +
+        bill_amt4_usd +
+        bill_amt5_usd +
+        bill_amt6_usd
+    ) AS total_bill_amt_usd,
+
+    (
+        pay_amt1_usd +
+        pay_amt2_usd +
+        pay_amt3_usd +
+        pay_amt4_usd +
+        pay_amt5_usd +
+        pay_amt6_usd
+    ) AS total_pay_amt_usd,
+
+    GREATEST(
+        pay_0,
+        pay_2,
+        pay_3,
+        pay_4,
+        pay_5,
+        pay_6
+    ) AS worst_payment_status,
+
+    default_flag
+
+FROM credit_usd;
