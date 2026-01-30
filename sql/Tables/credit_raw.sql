f3bb5f458cc7923a17ee0f55b0ff4f35958991d5
Philip Ray <philipxray@gmail.com>
2026-01-29 20:41:37 -0800

    Create credit_raw table for credit data storage
    Created a new table 'credit_raw' with various fields for credit data.

---
+++ sql/Tables/credit_raw.sql
@@ -0,0 +1,36 @@
+CREATE TABLE credit_raw (
+    id INTEGER,
+    limit_bal INTEGER,
+    sex INTEGER,
+    education INTEGER,
+    marriage INTEGER,
+    age INTEGER,
+
+    pay_0 INTEGER,
+    pay_2 INTEGER,
+    pay_3 INTEGER,
+    pay_4 INTEGER,
+    pay_5 INTEGER,
+    pay_6 INTEGER,
+
+    bill_amt1 INTEGER,
+    bill_amt2 INTEGER,
+    bill_amt3 INTEGER,
+    bill_amt4 INTEGER,
+    bill_amt5 INTEGER,
+    bill_amt6 INTEGER,
+
+    pay_amt1 INTEGER,
+    pay_amt2 INTEGER,
+    pay_amt3 INTEGER,
+    pay_amt4 INTEGER,
+    pay_amt5 INTEGER,
+    pay_amt6 INTEGER,
+
+    default_payment_next_month INTEGER
+);
+
+NULLIF(
+  regexp_replace(id, '[^0-9]', '', 'g'),
+  ''
+)::INTEGER
